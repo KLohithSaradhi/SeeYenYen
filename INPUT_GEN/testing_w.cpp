28e6534a35e39111b59c0c01941bc05dddf103bd
Lohith <lohith21110257@snuchennai.edu.in>
2024-03-27 22:25:13 +0530

    Test files for reference

--- INPUT_GEN/testing_w.cpp
+++ INPUT_GEN/testing_w.cpp
@@ -6,11 +6,14 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 
-    double arr[3] = {1.0, 2.0, 3.0};
-
+    double arr[9];
+    for (int i = 0; i < 9; i++)
+    {
+        arr[i] = (double)i;
+    }
 
     ofstream write_file("out.bin", std::ios::binary);
     cereal::BinaryOutputArchive archive( write_file );
 
-    archive( cereal::binary_data( arr, sizeof(double) * 3 ) );
+    archive( cereal::binary_data( arr, sizeof(double) * 9 ) );
 }
